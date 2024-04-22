88d7540c2c2a9ed85732e68c62c36f02ad29a90f
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-04-22 18:40:45 -0400

    Changed theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import daisyui from 'daisyui'
+import plugin from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -9,10 +10,33 @@ export default {
   theme: {
     extend: {},
   },
-
+  plugins: [
+    plugin,
+    daisyui,
+  ],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "cyberpunk"],
+    //darkTheme: false,
+    themes: [
+      {
+        light: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+          fontFamily: 'Quenda'
+        },
+      },
+      {
+        dark: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#383838",
+          fontFamily: 'Quenda'
+        },
+      },
+    ],
   },
-  
-  plugins: [daisyui],
 }
