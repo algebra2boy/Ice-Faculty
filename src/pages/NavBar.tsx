b9edd22bf545fadf60821b15f63e3e6415b38b34
Yongye Tan <yongyetan1209@gmail.com>
2024-04-22 18:38:48 +0000

    Add basic nav bar

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,9 +1,22 @@
 import { Outlet } from "react-router-dom";
+import DarkModeIcon from "../components/LightDarkMode";
 
 const NavBar = () => {
   return (
     <div>
-      <div>This is the nav bar</div>
+      <nav className="navbar bg-base-100">
+        <div className="flex-1">
+          {/* TODO: add logo here */}
+          <a className="btn btn-ghost text-xl">iceFaculty</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            {/* TODO: Only show upload button when user logins */}
+            <li><a href="/upload">Upload</a></li>
+          </ul>
+          <DarkModeIcon></DarkModeIcon>
+        </div>
+      </nav>
       <Outlet />
     </div>
   );
