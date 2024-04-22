99eca9c292dcf43acba3e0316dc1950978fc35fb
Yongye Tan <yongyetan1209@gmail.com>
2024-04-22 01:21:27 -0400

    add inital layout of upload page

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,20 +1,21 @@
-import { Outlet } from "react-router-dom";
-import DarkModeIcon from "../components/LightDarkMode";
+import { Outlet, Link } from "react-router-dom";
+import logo from "/favicon.svg";
+// import DarkModeIcon from "../components/LightDarkMode";
 
 const NavBar = () => {
   return (
     <div>
       <nav className="navbar bg-base-100">
         <div className="flex-1">
-          {/* TODO: add logo here */}
-          <a className="btn btn-ghost text-xl">iceFaculty</a>
+          <img src={logo} width={20} height={20}></img>
+          <p className="btn btn-ghost text-xl">iceFaculty</p>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             {/* TODO: Only show upload button when user logins */}
-            <li><a href="/upload">Upload</a></li>
+            <li><Link to="/upload">Upload</Link></li>
           </ul>
-          <DarkModeIcon></DarkModeIcon>
+          {/* <DarkModeIcon></DarkModeIcon> */}
         </div>
       </nav>
       <Outlet />
--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,5 +1,52 @@
+import { departmentOptions } from '../models/departmentOptions';
+import { useState } from 'react';
+
 const UploadPage = () => {
-  return <div>This is Upload</div>;
+
+  const [uploadOfficeHour, setUploadOfficeHour] = useState({
+    department: "",
+    courseNumber: "",
+    startDate: "",
+    endDate: "",
+    facultyName: ""
+  });
+
+  const valueHandler = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setUploadOfficeHour({ ...uploadOfficeHour, [name]: value });
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <div className="mx-auto w-1/3 bg-gray-100 p-6 rounded-lg shadow-lg flex flex-col gap-3 items-center">
+
+        <select className="select select-bordered select-sm w-full max-w-xs" defaultValue={"DEFAULT"}>
+          <option value="DEFAULT" disabled>Course Department</option>
+          {departmentOptions.map((department: string, index: number) => <option key={index}>{department}</option>)}
+        </select>
+
+        <input type="text" placeholder="Course Number" className="input input-bordered w-full max-w-xs" />
+
+        <DateTextField labelName="Start date" />
+
+        <DateTextField labelName="End date" />
+
+        <input type="text" placeholder="Faculty Name" className="input input-bordered w-full max-w-xs" />
+
+      </div>
+    </div>
+  );
+};
+
+const DateTextField = (props: { labelName: string }) => {
+  return (
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">{props.labelName}</span>
+      </div>
+      <input type="date" placeholder="Date" className="input input-bordered w-full max-w-xs" />
+    </label>
+  );
 };
 
 export default UploadPage;
