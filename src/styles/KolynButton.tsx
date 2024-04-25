cf8264eab4d2dc0bece80271662ba4364f3de861
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-04-24 22:37:00 -0400

    Now Kolyn Button's text color is adaptive to theme

--- src/styles/KolynButton.tsx
+++ src/styles/KolynButton.tsx
@@ -6,9 +6,9 @@ interface ButtonProps {
 }
 
 export const KolynButton = (props: ButtonProps): ReactElement => {
-  const style = props.isResponsive ? 
-    `w-24 md:w-32 lg:w-40 rounded-md bg-mainColor hover:bg-disableColor text-primaryColor min-h-12`:
-    `w-40 rounded-md bg-mainColor hover:bg-disableColor text-primaryColor min-h-12`;
+  const common = `text-xl rounded-md bg-mainColor hover:bg-disableColor text-base-100 min-h-12`;
+  const modify = props.isResponsive ? `w-24 md:w-32 lg:w-40 ` : `w-40 `;
+  const style = modify + " " + common;
 
   return (
     <button className={style}>
