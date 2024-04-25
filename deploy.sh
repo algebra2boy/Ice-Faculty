bbb91a26b95c603a3742e249a6055fbd90501c93
Haijie Qin <jackqin2000@gmail.com>
2024-04-25 18:31:25 -0400

    Add deployment setup script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+dockerUserName=$1
+
+if [ -z "$dockerUserName" ]; then
+  echo "Error: No input name provided for Docker repository."
+  exit 1
+fi
+
 docker build -t ice-faulty:latest .
 docker run -d -p 80:5173 ice-faulty:latest
+
+docker tag ice-faulty:latest "${dockerUserName}/ice-faulty:latest"
+docker login
+
+docker push "${dockerUserName}/ice-faulty:latest"
